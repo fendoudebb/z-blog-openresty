1b343636df9ed0f72d7ca03aa8ee244041ce9be6
fendoudebb <15317832520@163.com>
2020-02-24 10:45:18 +0800

    定时任务

---
+++ src/schedule/task.lua
@@ -0,0 +1,16 @@
+local delay = 2000  -- in seconds
+local task = function(premature)
+    if not premature then
+        local running_count = ngx.timer.running_count()
+        local pending_count = ngx.timer.pending_count()
+        ngx.log(ngx.ERR, "test ngx.timer.every#running_count=", running_count, ", pending_count=", pending_count)
+    end
+end
+
+if 0 == ngx.worker.id() then
+    local ok, err = ngx.timer.every(delay, task)
+    if not ok then
+        ngx.log(ngx.ERR, "failed to create timer: ", err)
+        return
+    end
+end
