481fa28d278f33778c2b066c6decf8cc62614396
fendoudebb <15317832520@163.com>
2020-03-07 13:09:58 +0800

    文章查询sql格式化

--- src/content/web/post.lua
+++ src/content/web/post.lua
@@ -1,22 +1,23 @@
 -- to_char(post_ts, 'YYYY-MM-DD HH24:MI:SS') as post_ts
--- select * , post_like @> '[{"ip":"117.187.27.178"}]'::jsonb from post; 判断json对象数组中是否包含
-local result = db.query([[
+-- 判断json对象数组中是否包含，post_like 为空时是null，不为空时是true/false
+-- select * , post_like @> '[{"ip":"117.187.27.178"}]' from post;
+
+local sql = [[
 select id, title, pv from post where post_status = 0 order by random() limit 10;
 select
 id, title, keywords, description, topics, content_html, word_count, post_status,
 pv, like_count, comment_count, comment_status, to_char(create_ts, 'YYYY-MM-DD') as create_ts, post_comment,
-post_like @> '[{"ip":"]] .. ngx.ctx.client_ip .. [["}]'::jsonb as is_liked
-from post where id = ]] .. ngx.var[1])
+post_like @> '[{"ip":"%s"}]'::jsonb as is_liked
+from post where id = %d and post_status = 0
+]]
+
+local result = db.query(string.format(sql, ngx.ctx.client_ip, ngx.var[1]))
 
 if result[2][1] == nil then
     ngx.exit(ngx.HTTP_NOT_FOUND)
 end
 
 local post = result[2][1]
-
-if post.post_status ~= 0 then
-    ngx.exit(ngx.HTTP_NOT_FOUND)
-end
 
 post.random_post = result[1]
 
