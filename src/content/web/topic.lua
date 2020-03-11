2a5410ba35849703f2578417a1262e52c90d14f7
fendoudebb <15317832520@163.com>
2020-03-11 15:06:06 +0800

    分页变量抽取

--- src/content/web/topic.lua
+++ src/content/web/topic.lua
@@ -1,22 +1,8 @@
 local template = require "resty.template"
 local db = require "module.db"
+local req = require "module.req"
 
-local args = ngx.req.get_uri_args()
-local page = 1;
-if args then
-    local arg_page = args.page
-    if type(arg_page) == "string" then
-        page = tonumber(arg_page)
-    end
-end
-
-if page < 1 then
-    page = 1
-end
-
-local size = 20;
-
-local offset = (page - 1) * size
+local sql_args = req.get_page_size(ngx.req.get_uri_args())
 
 local topic = ngx.var[1]
 local escape_topic = db.val_escape(topic)
@@ -26,6 +12,11 @@ select id, title, description, pv, like_count, comment_count, topics, to_char(cr
 select count(*) as count from post where post_status = 0 and %s=ANY(topics);
 ]]
 
-local result = db.query(string.format(sql, escape_topic, size, offset, escape_topic))
+local result = db.query(string.format(sql, escape_topic, sql_args.limit, sql_args.offset, escape_topic))
 
-template.render("topic.html", { posts = result[1], topic = topic, cur_page = page, sum_page = math.ceil(result[2][1].count / size) })
+template.render("topic.html", {
+    posts = result[1],
+    topic = topic,
+    cur_page = sql_args.page,
+    sum_page = math.ceil(result[2][1].count / sql_args.limit)
+})
