8f22f27ab0ac339cdbf7ecab89c0079c347a6c68
fendoudebb <15317832520@163.com>
2020-03-14 10:02:28 +0800

    英语角功能

--- src/content/web/english.lua
+++ src/content/web/english.lua
@@ -1,3 +1,26 @@
 local template = require "resty.template"
+local db = require "module.db"
+local req = require "module.req"
 
-template.render("english.html", { message = "Hello, English!" })
+local sql_args = req.get_page_size(ngx.req.get_uri_args())
+
+local sql = "select word, english_phonetic, american_phonetic, translation, example_sentence, sentence_translation, source, synonyms from english order by create_ts desc limit %d offset %d"
+
+local result = db.query(string.format(sql, sql_args.limit, sql_args.offset))
+
+local memory = ngx.shared.memory
+-- TODO 新增留言时记得重新设置
+local english_count = memory:get("english_count")
+if not english_count then
+    english_count = db.query("select count(*) as count from english")[1].count
+    memory:set("english_count", english_count)
+end
+
+
+template.render("english.html", {
+    title = "英语角",
+    keywords = "英语角，IT英语",
+    english = result,
+    cur_page = sql_args.page,
+    sum_page = math.ceil(english_count / sql_args.limit)
+})
