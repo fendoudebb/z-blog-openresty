f34a9a37302dc95c68fb2c23520a1fc472081e99
fendoudebb <15317832520@163.com>
2020-03-26 20:14:45 +0800

    根据id查列表

--- src/content/dashboard/post.lua
+++ src/content/dashboard/post.lua
@@ -10,12 +10,19 @@ local rank_type = ngx.ctx.body_data.rank_type
 
 
 local sql = [[
-select count(*) as count from post;
-select id, title, pv, post_status, prop, like_count, comment_count, topics, to_char(create_ts, 'YYYY-MM-DD hh24:MI:ss') as create_ts from post order by create_ts desc limit %d offset %d
+select count(*) as count from post %s;
+select id, title, pv, post_status, prop, like_count, comment_count, topics, to_char(create_ts, 'YYYY-MM-DD hh24:MI:ss') as create_ts from post %s order by create_ts desc limit %d offset %d
 ]]
 
+local where_cause = ""
 
-local result = db.query(string.format(sql, sql_args.limit, sql_args.offset))
+if type(post_id) == "number" then
+    where_cause = "where id = " .. post_id
+end
+
+ngx.log(ngx.ERR, where_cause)
+
+local result = db.query(string.format(sql, where_cause, where_cause, sql_args.limit, sql_args.offset))
 
 ngx.say(json.encode(const.ok({
     count = result[1][1].count,
