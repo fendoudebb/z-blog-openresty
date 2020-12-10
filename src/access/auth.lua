29d204d9797820e2d70133083bc3a0e16b1e3a0c
fendoudebb <15317832520@163.com>
2020-12-10 11:18:47 +0800

    fix

--- src/access/auth.lua
+++ src/access/auth.lua
@@ -17,7 +17,7 @@ local login_key = "dashboard-login-" .. token
 local user_info_json_str = memory:get(login_key)
 if not user_info_json_str then
     ngx.log(ngx.ERR, "auth missing login info, token#" .. token)
-    return ngx.say(json.encode(req.unauthorized()))
+    return req.unauthorized()
 end
 
 local user_info = json.decode(user_info_json_str)
@@ -26,12 +26,12 @@ local client_ip = req.get_ip_from_headers(ngx.req.get_headers())
 
 if user_info.ip ~= client_ip then
     ngx.log(ngx.ERR, "auth ip mismatch, login ip#" .. user_info.ip .. ", request ip#" .. client_ip)
-    return ngx.say(json.encode(req.unauthorized()))
+    return req.unauthorized()
 end
 
 if user_info.ua ~= ngx.var.http_user_agent then
     ngx.log(ngx.ERR, "auth ua mismatch, login ua#" .. user_info.ua .. ", request ip#" .. ngx.var.http_user_agent)
-    return ngx.say(json.encode(req.unauthorized()))
+    return req.unauthorized()
 end
 
 memory:expire(login_key, 604800)
