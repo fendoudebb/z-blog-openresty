13c46fd64a6ba9b5bd365ffa0f8b08a71eabd3cb
fendoudebb <15317832520@163.com>
2021-02-16 11:51:29 +0800

    ip_pool count slow query

--- src/content/dashboard/ip_pool.lua
+++ src/content/dashboard/ip_pool.lua
@@ -12,17 +12,22 @@ if req_url == "list" then
     local ip = ngx.ctx.body_data.ip
 
     local sql = [[
-        select count(*) as count from ip_pool %s;
+        %s
         select id, ip::text, country, region, city, isp, country_id, region_id, city_id, isp_id, to_char(create_ts, 'YYYY-MM-DD hh24:MI:ss') as create_ts from ip_pool %s order by id desc limit %d offset %d
     ]]
 
+    local count_sql
     local where_cause = ""
 
     if type(ip) == "string" then
+        count_sql = "select count(*) as count from ip_pool %s;"
         where_cause = "where ip=" .. db.quote(ip) .. "::inet"
+    else
+        count_sql = "select last_value as count from ip_pool_id_seq;"
     end
 
-    local result = db.query(string.format(sql, where_cause, where_cause, sql_args.limit, sql_args.offset))
+    sql = string.format(sql, count_sql, where_cause, sql_args.limit, sql_args.offset)
+    local result = db.query(sql)
 
     ngx.say(json.encode(const.ok({
         count = result[1][1].count,
